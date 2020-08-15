4cf76b86ffc60e947e2e0073176858c635db417d
Mykola Fedurko <mykolaf@compatibl.com>
2020-08-15 23:55:55 +0300

    feat(ext): added UBetterSquash extension

---
+++ UBetterSquash/content.js
@@ -0,0 +1,18 @@
+const randSquashMsg = () => {
+    const msgs = [
+        "Don't forget to squash, honey",
+        "U better squash",
+        "А засквошить?",
+        "You didn't squashed, do you?",
+        "Squash or not to squash? That's the question",
+        "Squash please",
+        "Love you, for squashing me",
+        "It tastes better if it's squashed"
+    ];
+
+    return msgs[Math.trunc(Math.random() * msgs.length + 1)];
+};
+
+document.getElementsByClassName("merge-button")[0].addEventListener(
+    "click", (event) => !confirm(randSquashMsg()) || event.stopPropagation()
+);
