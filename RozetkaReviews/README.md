e4ca4043c7405e27f827406605b3f0c7e2979d9e
Mykola Fedurko <kolausf@gmail.com>
2023-01-31 13:39:50 +0200

    Added rozetka reviews extension.

---
+++ RozetkaReviews/README.md
@@ -0,0 +1,3 @@
+# RozetkaReviews
+
+Chrome extension that sorts the reviews starting from the most helpful on a rozetka.com.ua website. 
---
+++ RozetkaReviews/content.js
@@ -0,0 +1,2 @@
+let reviews_button = document.getElementsByClassName("product-comments__sort")[0];
+reviews_button.children[1].value = 'helpful';
